fb71d8474595919474e8993a5b7d9fcb76700b1a
Carlos Bejarano <cabejarano21@gmail.com>
2022-10-04 23:02:13 -0500

    feat trabajando con JQuery

--- Curso-JavaScript/sesion-14-html/index.js
+++ Curso-JavaScript/sesion-14-html/index.js
@@ -7,3 +7,17 @@ console.log(a*b)
 const p = document.getElementById("parrafo")
 
 console.log(p)
+
+
+/* $(document).ready(function(){
+    $("h2").hide()
+}) */
+
+$(() =>{
+    $(".hide-btn").click(()=>{
+        $("h1").fadeOut()
+    })
+    $(".show-btn").click(()=>{
+        $("h1").fadeIn()
+    })
+})
